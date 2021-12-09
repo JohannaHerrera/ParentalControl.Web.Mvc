cf6246bec469d4c0a0118923b693f346757d2194
Kevin David Anchapaxi Moreta <kevin.anchapaxi@udla.edu.ec>
2021-12-08 21:15:24 -0500

    HorariosWeb

---
+++ ParentalControl.Web.Mvc/Controllers/ScheduleController.cs
@@ -0,0 +1,42 @@
+﻿using ParentalControl.Web.Mvc.Data;
+using ParentalControl.Web.Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace ParentalControl.Web.Mvc.Controllers
+{
+    public class ScheduleController : BaseController
+    {
+        //List Schedule
+        public ActionResult Index()
+        {
+            try
+            {
+                List<ScheduleModel> scheduleList = new List<ScheduleModel>();
+                
+                var parent = this.GetCurrentUserInfo();
+                using (var db = new ParentalControlDBEntities())
+                {
+                    scheduleList = (from schedule in db.Schedule
+                                    where schedule.ParentId == parent.Id
+                                    select new ScheduleModel
+                                    {
+                                        ScheduleId = schedule.ScheduleId,
+                                        ScheduleStartTime = schedule.ScheduleStartTime,
+                                        ScheduleEndTime = schedule.ScheduleEndTime,
+                                        ParentId = schedule.ParentId
+                                    }).ToList();
+                }
+                ViewBag.list= scheduleList;
+                return View(scheduleList);
+
+            }
+            catch(Exception ex)
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ParentalControl.Web.Mvc/Models/ScheduleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ParentalControl.Web.Mvc.Models
+{
+    public class ScheduleModel
+    {
+        
+        public int ScheduleId { get; set; }
+        public DateTime ScheduleStartTime { get; set; }
+        public DateTime ScheduleEndTime { get; set; }
+        public DateTime ScheduleCreationDate { get; set; }
+        public int ParentId { get; set; }
+
+    }
+}
